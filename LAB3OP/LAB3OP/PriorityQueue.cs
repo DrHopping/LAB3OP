bd10bc8edd5d7e4479522472178e1c0d9cf8f8c6
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-22 22:52:45 +0200

    Added Dequeue,Enqueue

--- LAB3OP/LAB3OP/PriorityQueue.cs
+++ LAB3OP/LAB3OP/PriorityQueue.cs
@@ -9,6 +9,69 @@
             public Node next;
         }
 
+        private Node back;
 
+        private bool Compare(Node prev, Node next)
+        {
+            return prev.priority > next.priority;
+        }
+
+        public void Enqueue(T value, int valuePriority)
+        {
+            Node temp = new Node();
+            temp.data = value;
+            temp.priority = valuePriority;
+            temp.next = null;
+
+            if (back == null)
+            {
+                back = temp;
+                return;
+            }
+
+            Node p = back, prev = null;
+            for (; p != null && Compare(temp, p); prev = p, p = p.next) ;
+
+            if (p == back)
+            {
+                temp.next = back;
+                back = temp;
+                return;
+            }
+
+            prev.next = temp;
+            if (p == null) return;
+
+            temp.next = p;
+        }
+
+        public T Dequeue()
+        {
+            T value;
+
+            if (back == null)
+            {
+                throw new System.Exception("Queue is empty");
+            }
+
+            if (back.next == null)
+            {
+                value = back.data;
+                back = null;
+                return value;
+            }
+
+            Node prev, end;
+            prev = end = back;
+            while (end.next != null)
+            {
+                prev = end;
+                end = end.next;
+            }
+
+            value = end.data;
+            prev.next = null;
+            return value;
+        }
     }
 }
