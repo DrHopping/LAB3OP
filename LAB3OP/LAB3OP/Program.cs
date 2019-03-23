668c880b39c3f6e97e743888a4bd7dcea3d802f3
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-23 17:25:00 +0200

    Moved AStarPathfind to AStarPathfind.cs

--- LAB3OP/LAB3OP/Program.cs
+++ LAB3OP/LAB3OP/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,112 +6,6 @@ using System.IO;
 
 namespace LAB3OP
 {
-
-    class AStarPathfind
-    {
-        Node start;
-        Node finish;
-        int[,] map;
-
-        PriorityQueue<Node> openList = new PriorityQueue<Node>();
-        List<Node> closedList = new List<Node>();
-
-
-        public AStarPathfind(Node start, Node finish, int[,] map)
-        {
-            this.start = start;
-            this.finish = finish;
-            this.map = map;
-            start.gCost = 0;
-            start.hCost = CalcHeuristic(start);
-            start.totalCost = start.gCost + start.hCost;
-        }
-
-        private int CalcHeuristic(Node location)
-        {
-            return Math.Abs(finish.x - location.x) + Math.Abs(finish.y - location.y);
-        }
-
-        private List<Node> Successors(Node location)
-        {
-            List<Node> successors = new List<Node>();
-
-            if (location.x - 1 > 0 && map[location.y, location.x - 1] != -1)
-                successors.Add(new Node(location.x - 1, location.y));
-
-            if (location.x + 1 < map.GetLength(1) && map[location.y, location.x + 1] != -1)
-                successors.Add(new Node(location.x + 1, location.y));
-
-            if (location.y - 1 > 0 && map[location.y - 1, location.x] != -1)
-                successors.Add(new Node(location.x, location.y - 1));
-
-            if (location.y + 1 < map.GetLength(0) && map[location.y + 1, location.x] != -1)
-                successors.Add(new Node(location.x, location.y + 1));
-
-            return successors;
-        }
-
-        private bool InClosedList(Node location)
-        {
-            foreach (var closed in closedList)
-            {
-                if (location.x == closed.x && location.y == closed.y)
-                    return true;
-            }
-            return false;
-        }
-
-        public bool FindPath()
-        {
-            openList.Enqueue(start, start.totalCost);
-            while(!openList.IsEmpty)
-            {
-                Node current = openList.Peek();
-
-                if (Node.Compare(finish, current))
-                    return true;
-
-                openList.Dequeue();
-                closedList.Add(current);
-
-                foreach (var location in Successors(current))
-                {
-                    //location.gCost = current.gCost + 1;
-                    //location.hCost = CalcHeuristic(location);
-                    //location.totalCost = location.gCost + location.hCost;
-                    int tentativeCost = current.gCost + 1;
-
-                    if (InClosedList(location) && tentativeCost >= location.gCost)
-
-                        continue;
-                    if(!InClosedList(location) || tentativeCost < location.gCost)
-                    {
-                        location.parent = current;
-                        location.gCost = tentativeCost;
-                        location.hCost = CalcHeuristic(location);
-                        location.totalCost = location.gCost + location.hCost;
-                        if (!InClosedList(location))
-                            openList.Enqueue(location, location.totalCost);
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        public void DrawPath()
-        {
-            Node current = openList.Peek();
-            int i = 1;
-            while(current != start)
-            {
-                map[current.y, current.x] = i;
-                current = current.parent;
-                i++;
-            }
-        }
-
-    }
 
     class Program
     {
